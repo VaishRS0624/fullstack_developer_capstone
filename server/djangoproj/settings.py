f0a867e9a1e53306dab3e89c3af64fcffc035f87
Vaishnavi Shinde <123016841+VaishRS0624@users.noreply.github.com>
2024-10-23 23:04:25 +0530

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -22,14 +22,26 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY =\
-    'django-insecure-ccow$tz_=9%dxu4(0%^(z%nx32#s@(zt9$ih@)5l54yny)wm-0'
+SECRET_KEY = 'django-insecure-ccow$tz_=9%dxu4(0%^(z%nx32#s@(zt9$ih@)'
+'5l54yny)wm-0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','https://vaishnavis19-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/']
-CSRF_TRUSTED_ORIGINS = ['https://vaishnavis19-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/']
+ALLOWED_HOSTS = [
+    'localhost',
+    'https://alexatzaraki-8000.theianext-0-labs-prod-misc-tools-us-east-0.'
+    'proxy.cognitiveclass.ai',
+    'https://alexatzaraki-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01'
+    '.proxy.cognitiveclass.ai',
+]
+CSRF_TRUSTED_ORIGINS = [
+    'https://alexatzaraki-8000.theianext-0-labs-prod-misc-tools-us-east-0.'
+    'proxy.cognitiveclass.ai',
+    'https://alexatzaraki-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01'
+    '.proxy.cognitiveclass.ai',
+
+]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
@@ -62,7 +74,9 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR,'frontend/static')
+            os.path.join(BASE_DIR, 'frontend/static'),
+            os.path.join(BASE_DIR, 'frontend/build'),
+            os.path.join(BASE_DIR, 'frontend/build/static'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -92,7 +106,8 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'django.contrib.auth.password_validation.'
+        'UserAttributeSimilarityValidator',
     },
     {
         'NAME':
@@ -135,8 +150,9 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'frontend/static')
-]
+    os.path.join(BASE_DIR, 'frontend/static'),
+    os.path.join(BASE_DIR, 'frontend/build'),
+    os.path.join(BASE_DIR, 'frontend/build/static'),
 
+]
