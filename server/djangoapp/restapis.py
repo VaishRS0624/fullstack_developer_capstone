fd0676dc601a16afb9d328005b0dddb4b54883ed
Vaishnavi Shinde <123016841+VaishRS0624@users.noreply.github.com>
2024-10-23 23:07:24 +0530

    Update restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,22 +1,45 @@
-# Uncomment the imports below before you add the function code
-# import requests
 import os
+import requests  # Uncomment to import requests module
 from dotenv import load_dotenv
 
 load_dotenv()
 
-backend_url = os.getenv(
-    'backend_url', default="http://localhost:3030")
-sentiment_analyzer_url = os.getenv(
-    'sentiment_analyzer_url',
-    default="http://localhost:5050/")
+# Define backend and sentiment analyzer URLs
+BACKEND_URL = os.getenv('backend_url', default="http://localhost:3030")
+SENTIMENT_ANALYZER_URL = os.getenv('sentiment_analyzer_url',
+                                   default="http://localhost:5050/")
 
-# def get_request(endpoint, **kwargs):
-# Add code for get requests to back end
 
-# def analyze_review_sentiments(text):
-# request_url = sentiment_analyzer_url+"analyze/"+text
-# Add code for retrieving sentiments
+def get_request(endpoint, **kwargs):
+    params = "&".join([f"{key}={value}" for key,
+                       value in kwargs.items()]) if kwargs else ""
+    request_url = f"{BACKEND_URL}{endpoint}?{params}"
 
-# def post_review(data_dict):
-# Add code for posting review
+    print(f"GET from {request_url}")
+    try:
+        response = requests.get(request_url)
+        return response.json()
+    except Exception as e:
+        print(f"Network exception occurred: {e}")
+
+
+def analyze_review_sentiments(text):
+    print("nigga")
+    print(SENTIMENT_ANALYZER_URL)
+    request_url = f"{SENTIMENT_ANALYZER_URL}analyze/{text}"
+    try:
+        response = requests.get(request_url)
+        return response.json()
+    except Exception as e:
+        print(f"Unexpected error: {e}")
+        print("Network exception occurred")
+
+
+def post_review(data_dict):
+    request_url = BACKEND_URL+"/insert_review"
+    try:
+        response = requests.post(request_url, json=data_dict)
+        print(response.json())
+        return response.json()
+    except Exception as e:
+        print(f"An error occurred: {e}")
